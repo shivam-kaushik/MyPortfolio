a18860824ae9c60b18dd17456b82ecd36a623254
shivam-kaushik <shivamkaushik2k@gmail.com>
2023-09-27 22:05:59 -0400

    Pages CSS and contact form

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+// Name: Shivam Kaushik
+// Student Node.: 301363333
+// Date: 27th Sep 2023
+
 var express = require('express');
 var app = express();
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
-app.use('/', require('./routes/router'));
+app.use('/', require('./routes/index.router'));
+app.use(express.static('./public'));
+app.use(express.static("./node_modules"));
 
 app.listen(3000);
 console.log('Server running at http://localhost:3000/');
